8c1fff635dd007f39e023a20c5127459b2fc56dd
RP2 <RP2@users.noreply.github.com>
2025-08-08 10:43:00 -0700

    setup boilerplate with shadcn script first

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
-	},
-
-  integrations: [react()],
+  // @ts-ignore
+  vite: { plugins: [tailwindcss()] },
+  site: "https://arg.rileyperalta.com",
+  integrations: [react(), sitemap()],
 });
